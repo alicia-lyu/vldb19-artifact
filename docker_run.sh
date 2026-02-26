eeb3df44a0c074c88e1ce6076d92ab01ef6ab296
alicia-lyu <wlyu27@wisc.edu>
2026-02-26 15:17:50 -0700

    tweaks

--- docker_run.sh
+++ docker_run.sh
@@ -1,11 +1,13 @@
 docker pull ghcr.io/alicia-lyu/geodb-dbtoaster:latest
 DBTOASTER_ID=$(docker run -d ghcr.io/alicia-lyu/geodb-dbtoaster:latest)
+docker ps
 echo "Waiting for DBToaster container" $DBTOASTER_ID "to finish..."
 docker wait $DBTOASTER_ID
-docker cp $DBTOASTER_ID:/results/update_times.csv ./update_times.csv
+docker cp $DBTOASTER_ID:/app/update_times.csv ./update_times.csv
 
 docker pull ghcr.io/alicia-lyu/geodb:latest
 MAIN_ID=$(docker run -d ghcr.io/alicia-lyu/geodb:latest)
+docker ps
 echo "Waiting for main container" $MAIN_ID "to finish..."
 docker wait $MAIN_ID
 docker cp $MAIN_ID:/results/geo_btree_TPut.csv ./geo_btree/TPut.csv
